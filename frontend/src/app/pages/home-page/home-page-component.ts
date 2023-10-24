f5b4804253cec08daa6ec2c42528ee711a5d2dde
Łukasz Pośpiech <lukaszpospiech1337@gmail.com>
2023-10-24 23:49:46 +0200

    deploy na android studio

--- frontend/src/app/pages/home-page/home-page-component.ts
+++ frontend/src/app/pages/home-page/home-page-component.ts
@@ -12,35 +12,33 @@ export class HomePageComponent {
   selectedCategory!: string;
   constructor(private router: Router) { }
 
+
   ngOnInit() {
+    const sampleCardData = [
+      {
+        title: 'Angular',
+        src: '/assets/images/angular.png'
+      },
+      {
+        title: 'Ionic',
+        src: '/assets/images/ionic.svg'
+      },
+      {
+        title: 'Scss',
+        src: '/assets/images/scss.svg'
+      },
+      {
+        title: 'HTML',
+        src: '/assets/images/html.png'
+      }
+    ];
+
+    localStorage.setItem('favorites-category', JSON.stringify(sampleCardData));
     this.sampleCardData = JSON.parse(localStorage.getItem('favorites-category') || '[]');
     if (this.sampleCardData.length === 0) {
       return;
     }
     console.log(this.sampleCardData);
-
-    // const sampleCardData = [
-    //   {
-    //     title: 'Angular',
-    //     src: '/assets/images/angular.png'
-    //   },
-    //   {
-    //     title: 'Ionic',
-    //     src: '/assets/images/ionic.svg'
-    //   },
-    //   {
-    //     title: 'Scss',
-    //     src: '/assets/images/scss.svg'
-    //   },
-    //   {
-    //     title: 'HTML',
-    //     src: '/assets/images/html.png'
-    //   }
-    // ];
-
-    // localStorage.setItem('favorites-category', JSON.stringify(sampleCardData));
-
-
   }
 
   handleNewCardEvent(data: any,componentIdentifier: string) {
