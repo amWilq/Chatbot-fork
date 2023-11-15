7587e08608270cc7fa9034f29d5969d6d73c9c29
Łukasz Pośpiech <lukaszpospiech1337@gmail.com>
2023-11-15 20:01:03 +0100

    Add custom button and display contents in category-component.scss and fix typo in quiz-summary.component.ts

--- frontend/src/app/components/pick-answer-quiz/pick-answer-quiz.component.ts
+++ frontend/src/app/components/pick-answer-quiz/pick-answer-quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { QuizQuestion } from 'src/app/entities/quiz-question.model';
 
 @Component({
@@ -7,27 +7,45 @@ import { QuizQuestion } from 'src/app/entities/quiz-question.model';
   styleUrls: ['./pick-answer-quiz.component.scss']
 })
 export class PickAnswerQuizComponent {
-  @Input() questions: QuizQuestion[] = [];
-  @Output() answerSelected = new EventEmitter<{ selected: string; correct: boolean }>();
-  currentQuestionIndex: number = 0;
+  @Input() questions: any
+  @Output() quizCompleted: EventEmitter<void> = new EventEmitter<void>();
   selectedAnswer: string | null = null;
+  progress: number = 0;
+  displayTime: string | undefined;
+  timer: any | null = null;
 
-  selectAnswer(answer: string) {
-    this.selectedAnswer = answer;
-    const isCorrect = this.questions[this.currentQuestionIndex].correctAnswer === answer;
-    this.answerSelected.emit({ selected: answer, correct: isCorrect });
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    let time = 300; // Start from 300 seconds (5 minutes)
+    this.displayTime = this.formatTime(time);
+    this.timer = setInterval(() => {
+      time--;
+      this.progress = time / 300;
+      this.displayTime = this.formatTime(time);
+      if (time <= 0) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `Pozostało ${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
   }
 
   nextQuestion() {
-    if (this.currentQuestionIndex < this.questions.length - 1) {
-      this.currentQuestionIndex++;
-      this.selectedAnswer = null;
-    } else {
-      //show a results screen or reset the quiz
-    }
+    console.log('Next question');
+    this.quizCompleted.emit();
   }
 
   get currentQuestion(): QuizQuestion {
-    return this.questions[this.currentQuestionIndex];
+    return this.questions;
   }
 }
