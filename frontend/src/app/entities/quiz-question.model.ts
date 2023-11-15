7f65981922cf538b43bf01fd711890bd849be99f
Łukasz Pośpiech <lukaszpospiech1337@gmail.com>
2023-11-15 20:01:31 +0100

    Refactor quiz summary component and update quiz model

--- frontend/src/app/entities/quiz-question.model.ts
+++ frontend/src/app/entities/quiz-question.model.ts
@@ -1,5 +1,24 @@
 export interface QuizQuestion {
-  question: string;
+  answers: string[];
+  content: string;
+}
+
+interface Question {
+  content: string;
   answers: string[];
   correctAnswer: string;
+  explanation: string;
+  yourAnswer: string;
+  isCorrect: boolean;
+  takenTime: number;
+}
+
+export interface QuizModel {
+  answeredQuestions: number;
+  correctAnswers: number;
+  startDifficulty: string;
+  endDifficulty: string;
+  duration: number;
+  questions: Question[];
+  feedback: string;
 }
--- frontend/src/app/pages/quiz/quiz-component.ts
+++ frontend/src/app/pages/quiz/quiz-component.ts
@@ -10,29 +10,29 @@ export class QuizComponent {
 
   selectedAnswers: string[] = [];
   showSummary: boolean = false;
-  quizQuestions: QuizQuestion[] = [
-    {
-      question: "What is the capital of France?",
-      answers: ["Paris", "London", "Berlin", "Rome"],
-      correctAnswer: "Paris"
-    },
-    {
-      question: "Which element's chemical symbol is O?",
-      answers: ["Oxygen", "Gold", "Silver", "Iron"],
-      correctAnswer: "Oxygen"
-    },
-    {
-      question: "What is the largest planet in our Solar System?",
-      answers: ["Jupiter", "Mars", "Earth", "Venus"],
-      correctAnswer: "Jupiter"
-    }
-  ];
+  // quizQuestions: QuizQuestion[] = [
+  //   {
+  //     question: "What is the capital of France?",
+  //     answers: ["Paris", "London", "Berlin", "Rome"],
+  //     correctAnswer: "Paris"
+  //   },
+  //   {
+  //     question: "Which element's chemical symbol is O?",
+  //     answers: ["Oxygen", "Gold", "Silver", "Iron"],
+  //     correctAnswer: "Oxygen"
+  //   },
+  //   {
+  //     question: "What is the largest planet in our Solar System?",
+  //     answers: ["Jupiter", "Mars", "Earth", "Venus"],
+  //     correctAnswer: "Jupiter"
+  //   }
+  // ];
 
   handleAnswerSelected(event: { selected: string; correct: boolean }): void {
-    this.selectedAnswers.push(event.selected);
-    if (this.selectedAnswers.length === this.quizQuestions.length) {
-      this.showSummary = true;
-    }
+    // this.selectedAnswers.push(event.selected);
+    // if (this.selectedAnswers.length === this.quizQuestions.length) {
+    //   this.showSummary = true;
+    // }
   }
 
   onReplayQuiz() {
