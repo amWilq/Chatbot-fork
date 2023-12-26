7e45316038d311387a298ca77048e91e75df6498
Łukasz Pośpiech <lukaszpospiech1337@gmail.com>
2023-12-26 01:57:51 +0100

    Update server address and API URLs
    Remove unused code snippet module imports

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -1,5 +1,10 @@
-export const SERVER_ADDRES = 'api.codeperium.io/v2';
-export const SERVER_API_URL = `https://${SERVER_ADDRES}`;
+// export const SERVER_ADDRES = 'api.codeperium.io/v2';
+// export const SERVER_API_URL = `https://${SERVER_ADDRES}`;
+// export const WW_SERVER_API_URL = `ws://${SERVER_ADDRES}`;
 
 // export const SERVER_ADDRES = 'localhost:32768';
 // export const SERVER_API_URL = `http://${SERVER_ADDRES}`;
+
+export const SERVER_ADDRES = 'localhost';
+export const SERVER_API_URL = `http://${SERVER_ADDRES}/v2`;
+export const WW_SERVER_API_URL = `ws://${SERVER_ADDRES}/v2`;
--- frontend/src/app/components/code-snippet/code-snippet.module.ts
+++ frontend/src/app/components/code-snippet/code-snippet.module.ts
@@ -3,20 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CodeSnippetComponent } from './code-snippet.component';
-import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
 
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule,HighlightModule],
+  imports: [ CommonModule, FormsModule, IonicModule],
   declarations: [CodeSnippetComponent],
   exports: [CodeSnippetComponent],
-  // providers: [
-  //   {
-  //     provide: HIGHLIGHT_OPTIONS,
-  //     useValue: {
-  //       fullLibraryLoader: () => import('highlight.js'),
-  //     }
-  //   }
-  // ],
+
 })
 export class CodeSnippetComponentModule {}
+
